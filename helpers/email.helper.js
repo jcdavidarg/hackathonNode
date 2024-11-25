e0942a49888f7c2280ee32c4f87f8a4824fb346e
jcdavidarg <jcdavidarg@gmail.com>
2024-11-24 23:53:00 -0300

    helper para email

---
+++ helpers/email.helper.js
@@ -0,0 +1,33 @@
+import nodemailer from "nodemailer";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS } = process.env;
+
+const transporter = nodemailer.createTransport({
+    host: SMTP_HOST,
+    port: SMTP_PORT,
+    auth: {
+        user: SMTP_USER,
+        pass: SMTP_PASS
+    }
+});
+
+const sendMail = async (emailTo, subject, body) => {
+    try {
+        const email = {
+            from: SMTP_USER,
+            to: emailTo,
+            subject: subject,
+            text: body
+        };
+
+        await transporter.sendMail(email);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default sendMail;
